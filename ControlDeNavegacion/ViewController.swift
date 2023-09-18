9b43456c726b2e6ed853c4f41f75edb323d20899
Ara25cely <aracelyjacinto@gmail.com>
2023-09-17 18:24:05 -0600

    Agregando archivos

---
+++ ControlDeNavegacion/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ControlDeNavegacion
+//
+//  Created by CEDAM19 on 07/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Regresar a la vista inicial
+    @IBAction func regresarAPrincipal(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
